bd6249a8059186cdf50f82f49564039670b9c5f2
Hennadii Melnyk <162840010+gadiim@users.noreply.github.com>
2024-08-23 22:15:08 +0300

    generate service news create mock news

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 220824Pipes
+# 20.08.24_pipes
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.2.0.
 
---
+++ src/app/mock-news.ts
@@ -0,0 +1,9 @@
+export const NEWS = [
+    {
+        headline: 'Mystery Explosions Near Corvega: Raider Activity or Something Worse?',
+        description: 'Explosions and strange lights near the Corvega Assembly Plant have sparked fears of raider activity or a new threat. Locals are on edge, with rumors pointing to the Institute or super mutants. Mayor McDonough remains silent.',
+        body: 'Lexington, MA - Reports of loud explosions and strange lights near the Corvega Assembly Plant have locals on edge. Eyewitnesses claim to have seen shadowy figures moving near the plant\'s perimeter, sparking rumors of increased raider activity or even something more sinister. Some speculate the Institute might be behind the disturbances, while others fear it\'s a new breed of super mutants. Mayor McDonough has yet to comment on the situation. Stay vigilant, and keep your loved ones close.',
+        publicationDate: '2287-10-22T08-00-00',
+        image: 'https://images.fallout.wiki/c/c5/FO4_Corvega_assembly_plant_at_night.jpg'
+    }
+];
